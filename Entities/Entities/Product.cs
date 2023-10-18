c8719ee8b842a20e232b72b51e3bca755c56db72
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-17 21:45:38 -0300

    Criada classe de produto, base e notificações

---
+++ Entities/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("Product")]
+    public class Product : Base
+    {
+        [Column("PRD_PRECO")]
+        [Display(Name = "Preco")]
+        public decimal Price { get; set; }
+
+        [Column("PRD_ESTADO")]
+        [Display(Name = "Estado")]
+        public bool State { get; set; }
+    }
+}
---
+++ Entities/Notifications/Notifiers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notifications
+{
+    public class Notifiers
+    {
+        public Notifiers()
+        {
+            Notifications = new List<Notifiers>();
+        }
+
+        [NotMapped]
+        public string PropertyName { get; set; }
+
+        [NotMapped]
+        public string Message { get; set; }
+
+        [NotMapped]
+        public List<Notifiers> Notifications;
+
+        public bool ValidatePropertyString(string value, string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(propertyName))
+            {
+                Notifications.Add(new Notifiers { PropertyName = propertyName, Message = "Campo obrigatório" });
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidatePropertyInt(int value, string propertyName)
+        {
+            if (value < 1 || string.IsNullOrWhiteSpace(propertyName))
+            {
+                Notifications.Add(new Notifiers { PropertyName = propertyName, Message = "Valor deve ser  maior que 0" });
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidatePropertyDecimal(decimal value, string propertyName)
+        {
+            if (value < 1 || string.IsNullOrWhiteSpace(propertyName))
+            {
+                Notifications.Add(new Notifiers { PropertyName = propertyName, Message = "Valor deve ser  maior que 0" });
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
