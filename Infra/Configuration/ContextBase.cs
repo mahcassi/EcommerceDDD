e158a78d1eaa62ac86a1b1718ca777a7661e2ffa
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-19 21:18:45 -0300

    Criado o Contexto base

---
+++ Infra/Configuration/ContextBase.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConnectionConfig()
+        {
+            string strCon = "Server=(localdb)\\mssqllocaldb;Database=DDD_Ecommerce;Integrated Security=True;TrustServerCertificate=True";
+            return strCon;
+        }
+    }
+}
