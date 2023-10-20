ff70aa125af1c80f4c0e21479fa0247d41994b26
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-19 21:42:10 -0300

    Usando o IdentityDbContext no nosso Contexto

--- Infra/Configuration/ContextBase.cs
+++ Infra/Configuration/ContextBase.cs
@@ -1,14 +1,10 @@
 ﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infra.Configuration
 {
-    public class ContextBase : DbContext
+    public class ContextBase : IdentityDbContext<ApplicationUser>
     {
         public ContextBase(DbContextOptions<ContextBase> options) : base(options)
         {
