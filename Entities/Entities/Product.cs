b14c23cd3dc7252addf8fe725a02c5b4de8fb0cf
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-23 21:47:24 -0300

    Adicionando novas props na entidade produto

--- Entities/Entities/Product.cs
+++ Entities/Entities/Product.cs
@@ -12,12 +12,40 @@ namespace Entities.Entities
     [Table("Product")]
     public class Product : Base
     {
-        [Column("PRD_PRECO")]
-        [Display(Name = "Preco")]
-        public decimal Price { get; set; }
+        [Column("PRD_VALOR")]
+        [Display(Name = "Valor")]
+        public decimal Valor { get; set; }
+
+        [Column("PRD_QTD_ESTOQUE")]
+        [Display(Name = "Quantidade Estoque")]
+        public decimal QtdEstoque { get; set; }
+
+        [Column("PRD_DESCRICAO")]
+        [Display(Name = "Descrição")]
+        [MaxLength(150)]
+        public string Descricao { get; set; }
+
+        [Column("PRD_OBSERVACAO")]
+        [Display(Name = "Observacao")]
+        [MaxLength(150)]
+        public string Observacao { get; set; }
 
         [Column("PRD_ESTADO")]
         [Display(Name = "Estado")]
-        public bool State { get; set; }
+        public bool Estado { get; set; }
+
+        [Display(Name = "Usuário")]
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get;set; }
+
+        [Column("PRD_DATA_CADASTRO")]
+        [Display(Name = "Data de Cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        [Column("PRD_DATA_ALTERACAO")]
+        [Display(Name = "Data de Alteração")]
+        public DateTime DataAlteracao { get; set; }
     }
 }
