8e221a23c31ee160f92bdd27f2c5e53215e262ca
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-23 21:11:40 -0300

    Configuração do conexto no proram

--- Web_ECommerce/Program.cs
+++ Web_ECommerce/Program.cs
@@ -1,3 +1,7 @@
+using Entities.Entities;
+using Infra.Configuration;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkSqlServer()
+               .AddDbContext<ContextBase>(
+                  options =>
+                  {
+                      options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"));
+                      options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                  }
+               );
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ContextBase>();
 
 var app = builder.Build();
 
