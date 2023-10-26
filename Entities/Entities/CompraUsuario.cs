65e22315e463f9d87398c96abb5dbca823cc7127
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-25 22:10:52 -0300

    Criada entidade de "Compra Usuario"

---
+++ Entities/Entities/CompraUsuario.cs
@@ -0,0 +1,41 @@
+﻿using Entities.Entities.Enums;
+using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("TB_COMPRA_USUARIO")]
+    public class CompraUsuario : Notifiers
+    {
+        [Column("CUS_ID")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Produto")]
+        [ForeignKey("TB_PRODUTO")]
+        [Column(Order = 1)]
+        public int IdProduto { get; set; }
+        public virtual Product Produto { get; set; }
+
+        [Display(Name = "Usuário")]
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 2)]
+        public string UserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Column("CUS_ESTADO")]
+        [Display(Name = "Estado")]
+        public EEstadoCompra Estado { get; set; }
+
+        [Column("CSU_QTD")]
+        [Display(Name = "Quantidade")]
+        public int QtdCompra { get; set; }
+
+    }
+}
