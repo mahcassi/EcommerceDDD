24a6f0153286ff17267dcca6721aea9d066c9ba6
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-25 22:26:23 -0300

    Retirado imports

--- Entities/Entities/CompraUsuario.cs
+++ Entities/Entities/CompraUsuario.cs
@@ -1,12 +1,7 @@
 ﻿using Entities.Entities.Enums;
 using Entities.Notifications;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Entities.Entities
 {
