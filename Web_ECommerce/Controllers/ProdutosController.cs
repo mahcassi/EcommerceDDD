07232081c3eab3634d05e26136fe89b4fe2299b6
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-26 21:58:16 -0300

    Criada controller inicial de produto

---
+++ Web_ECommerce/Controllers/ProdutosController.cs
@@ -0,0 +1,85 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_ECommerce.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        
+        // GET: ProdutosController
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        // GET: ProdutosController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProdutosController/Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Produto produto)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProdutosController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Produto produto)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProdutosController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Produto produto)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
