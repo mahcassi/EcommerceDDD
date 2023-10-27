1fa9dc6f744d284fa801b30a7429daf7199eae9f
Maria Eduarda <mariaed.cassiano@gmail.com>
2023-10-27 14:50:59 -0300

    Estruturando controller de produto

--- Web_ECommerce/Controllers/ProdutosController.cs
+++ Web_ECommerce/Controllers/ProdutosController.cs
@@ -1,22 +1,29 @@
-﻿using Entities.Entities;
+﻿using Application.Interfaces;
+using Entities.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Web_ECommerce.Controllers
 {
     public class ProdutosController : Controller
     {
-        
+        protected readonly IProdutoApp _produtoApp;
+        public ProdutosController(IProdutoApp produtoApp)
+        {
+            _produtoApp = produtoApp;
+        }
+
         // GET: ProdutosController
         public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _produtoApp.List());
         }
 
         // GET: ProdutosController/Details/5
         public async Task<IActionResult> Details(int id)
         {
-            return View();
+            return View(await _produtoApp.GetEntityById(id));
         }
 
         // GET: ProdutosController/Create
@@ -32,18 +39,31 @@ namespace Web_ECommerce.Controllers
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                await _produtoApp.AddProduto(produto);
+
+                if (produto.Notifications.Any())
+                {
+                    foreach(var notification in produto.Notifications)
+                    {
+                        ModelState.AddModelError(notification.PropertyName, notification.Message);
+                    }
+
+                    return View("Edit", produto);
+                }
             }
             catch
             {
-                return View();
+                return View("Edit", produto);
             }
+
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: ProdutosController/Edit/5
         public async Task<IActionResult> Edit(int id)
         {
-            return View();
+            return View(await _produtoApp.GetEntityById(id));
+
         }
 
         // POST: ProdutosController/Edit/5
@@ -53,27 +73,43 @@ namespace Web_ECommerce.Controllers
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                await _produtoApp.UpdateProduto(produto);
+
+                if (produto.Notifications.Any())
+                {
+                    foreach (var notification in produto.Notifications)
+                    {
+                        ModelState.AddModelError(notification.PropertyName, notification.Message);
+                    }
+
+                    return View("Edit", produto);
+                }
             }
             catch
             {
-                return View();
+                return View("Edit", produto);
             }
+
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: ProdutosController/Delete/5
         public async Task<IActionResult> Delete(int id)
         {
-            return View();
+            return View(await _produtoApp.GetEntityById(id));
         }
 
         // POST: ProdutosController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, Produto produto)
+        public async Task<IActionResult> Delete(int id, Produto produto)
         {
             try
             {
+                var produtoDeletar = await _produtoApp.GetEntityById(id);
+
+                await _produtoApp.Delete(produtoDeletar);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
